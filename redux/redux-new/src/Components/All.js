6778fd506ee5741480d19ad667c0f2a4f0942427
Patrick Hill <graffixnyc@gmail.com>
2020-12-02 17:05:04 -0500

    Added more to redux example

--- redux/redux-new/src/Components/All.js
+++ redux/redux-new/src/Components/All.js
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
-
+import { useDispatch } from 'react-redux';
+import actions from '../actions';
 const All = () => {
+  const dispatch = useDispatch();
   const allState = useSelector((state) => state);
 
   return (
@@ -13,6 +15,17 @@ const All = () => {
           return <li key={todo.id}>{todo.task}</li>;
         })}
       </ul>
+      <br />
+      <button
+        onClick={() =>
+          dispatch(
+            actions.addTodo('Task Added From All Comp', 'How now Brown Cow')
+          )
+        }
+      >
+        Add Task
+      </button>
+      <hr />
       <h3>Users</h3>
       <ul>
         {allState.users.map((user) => {
@@ -20,6 +33,14 @@ const All = () => {
           return <li key={user.id}>{user.name}</li>;
         })}
       </ul>
+      <br />
+      <button
+        onClick={() =>
+          dispatch(actions.addUser('Carole Baskin', 'purrrfect@tigerking.com'))
+        }
+      >
+        Add User
+      </button>
     </div>
   );
 };
