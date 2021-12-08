acddc8ed3524a8a6535a7232490085923cc29800
Patrick Hill <graffixnyc@gmail.com>
2021-12-08 15:42:12 -0500

    Removed Axios calls from getStaticProps because making API calls directly is apparently bad (but calling a function that makes an API call is ok) ¯\_(ツ)_/¯

--- nextjs/pages/shows/[id].js
+++ nextjs/pages/shows/[id].js
@@ -27,15 +27,25 @@ export default function show({ data }) {
 }
 
 export async function getStaticProps({ params }) {
-	const { data } = await axios.get('http://api.tvmaze.com/shows/' + params.id);
-	console.log(`Fetched a show: ${data.name}`);
+	const data = await getIndShowData(params.id);
 	return {
-		props: { data }
+		props: { data },
+		revalidate: 86400
 	};
 }
 
-export async function getStaticPaths() {
+async function getIndShowData(id) {
+	const { data } = await axios.get('http://api.tvmaze.com/shows/' + id);
+	console.log(`Fetched a show: ${data.name}`);
+	return data;
+}
+
+async function getShowData() {
 	const { data } = await axios.get('http://api.tvmaze.com/shows');
+	return data;
+}
+export async function getStaticPaths() {
+	const data = await getShowData();
 	const paths = data.map((show) => {
 		return {
 			params: { id: show.id.toString() }
--- nextjs/pages/shows/index.js
+++ nextjs/pages/shows/index.js
@@ -2,39 +2,45 @@ import Layout from '../../components/MyLayout';
 import axios from 'axios';
 import Link from 'next/link';
 export default function shows({ data }) {
-  return (
-    <Layout>
-      <h1>Shows (getStaticProps)</h1>
-      <ul>
-        {data.map((show) => (
-          <li key={show.id}>
-            <Link href={`/shows/${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <h1>Shows (getServerSideProps)</h1>
-      <ul>
-        {data.map((show) => (
-          <li key={show.id}>
-            <Link href={`/show/${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <style jsx>{`
-        ul {
-          list-style-type: none;
-        }
-      `}</style>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<h1>Shows (getStaticProps)</h1>
+			<ul>
+				{data.map((show) => (
+					<li key={show.id}>
+						<Link href={`/shows/${show.id}`}>
+							<a>{show.name}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+			<h1>Shows (getServerSideProps)</h1>
+			<ul>
+				{data.map((show) => (
+					<li key={show.id}>
+						<Link href={`/show/${show.id}`}>
+							<a>{show.name}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+			<style jsx>{`
+				ul {
+					list-style-type: none;
+				}
+			`}</style>
+		</Layout>
+	);
+}
+
+async function getShowData() {
+	const { data } = await axios.get('http://api.tvmaze.com/shows');
+	return data;
 }
 export async function getStaticProps() {
-  const { data } = await axios.get('http://api.tvmaze.com/shows');
-  return {
-    props: { data }
-  };
+	const data = await getShowData();
+	return {
+		props: { data },
+		revalidate: 86400
+	};
 }
